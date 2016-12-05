61e3d47384125b47ecc4ad480117bb26977f628d
Thomas Rune Sørensen <thom009a@easv365.dk>
2016-12-05 23:25:05 +0100

    Tilføjede AlfabetSpil
    Tilføjede AlfabetSpil og bogstaver assets

---
+++ FessorKanin/Assets/Scripts/AlphabetGameManger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlphabetGameManger : MonoBehaviour {
+
+    public Sprite[] Letters;
+    public Sprite[] Options;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
